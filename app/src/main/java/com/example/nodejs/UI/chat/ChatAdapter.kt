e39bf46403e045dff47937168804dfed8b241ff7
leetaehoon <lyc9290@gmail.com>
2021-02-26 14:27:39 +0900

    In chatting room, when user click opponent profile image pop up profile page

--- app/src/main/java/com/example/nodejs/UI/chat/ChatAdapter.kt
+++ app/src/main/java/com/example/nodejs/UI/chat/ChatAdapter.kt
@@ -3,9 +3,13 @@ package com.example.nodejs.UI.chat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.nodejs.Model.Message
 import com.example.nodejs.R
+import com.example.nodejs.UI.profile.ProfileFragmentDirections
+import com.example.nodejs.glide.GlideApp
 import kotlinx.android.synthetic.main.item_chat_receiver.view.*
 import kotlinx.android.synthetic.main.item_chat_sender.view.*
 
@@ -44,9 +48,20 @@ class ChatAdapter(val name : String) : RecyclerView.Adapter<ChatAdapter.ChatView
             itemView.apply {
                 when (itemViewType) {
                     RECEIVER -> {
+                        GlideApp.with(chatReceiverImage)
+                            .load(R.drawable.my_image)
+                            .transform(CircleCrop())
+                            .thumbnail(0.1f)
+                            .into(chatReceiverImage)
                         receiverName.text = message.sender
                         txtReceiverMessage.text = message.message
                         txtReceiverDateTime.text = message.timeStamp
+
+                        chatReceiverImage.setOnClickListener {
+                            val direction =
+                                ProfileFragmentDirections.actionGlobalProfileFragment(message.sender, true)
+                            findNavController().navigate(direction)
+                        }
                     }
                     SENDER -> {
                         txtSenderMessage.text = message.message
